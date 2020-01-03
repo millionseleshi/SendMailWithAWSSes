8a4487d7134a2a2034f935ef46042f409e8c9c0a
millionseleshi <million2seleshi@gmail.com>
2020-01-03 20:11:03 +0300

    Send Mail with spring-boot and AWS SES

--- src/main/java/com/liqtech/sendmailwithaws/Controller/MailController.java
+++ src/main/java/com/liqtech/sendmailwithaws/Controller/MailController.java
@@ -1,37 +1,16 @@
-/*
- * Copyright (c) 2019. MILLION SELESHI
- * License: MIT
- *  Permission is hereby granted, free of charge, to any person obtaining a
- *  copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
- *  .
- *  The above copyright notice and this permission notice shall be included
- *  in all copies or substantial portions of the Software.
- *  .
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package com.liqtech.sendmailwithaws.Controller;
 
 import com.liqtech.sendmailwithaws.Config.CustomPropertyConfig;
 import com.liqtech.sendmailwithaws.Mail;
 import com.liqtech.sendmailwithaws.Service.EmailSenderService;
+import java.util.HashMap;
+import java.util.Map;
+import javax.mail.MessagingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController public class MailController {
+@RestController
+public class MailController {
 
 	private EmailSenderService emailSenderService;
 	private CustomPropertyConfig customPropertyConfig;
@@ -41,18 +20,22 @@ import java.util.Map;
 		this.customPropertyConfig = customPropertyConfig;
 	}
 
-	@GetMapping(value = "/send") public String sendMail() {
+	@GetMapping(value = "/send")
+	public String sendMail() throws MessagingException {
+		Mail mail = getMail();
+		emailSenderService.sendEmail(mail);
+		return "Check your email";
 
+	}
+
+	private Mail getMail() {
 		Mail mail = new Mail();
 		mail.setFrom(customPropertyConfig.mailFrom);
-		mail.setTo("<dummy@gmail.com>");
+		mail.setTo("million2seleshi@gmail.com");
 		mail.setSubject("Simple mail");
 		Map<String, Object> model = new HashMap<>();
 		model.put("templateVariable", "Simple mail with aws..");
 		mail.setModel(model);
-		emailSenderService.sendEmail(mail);
-
-		return "Check your email";
-
+		return mail;
 	}
 }
